5229fa6fb78ecb14c6beb9569b6d125d17ddfe64
Soojin <psjinqkr23@gmail.com>
2023-01-03 22:15:57 +0900

    [psj] start.spring.io project init

---
+++ src/test/java/kr/co/challengers/ChallengersApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.challengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
